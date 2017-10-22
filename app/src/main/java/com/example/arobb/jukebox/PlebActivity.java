d5eeb88cff291ce77324a78770f8568b7588c570
trobb10 <amr2146@gmail.com>
2017-10-21 20:32:18 -0400

    initial commit

---
+++ app/src/main/java/com/example/arobb/jukebox/PlebActivity.java
@@ -0,0 +1,14 @@
+package com.example.arobb.jukebox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlebActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pleb);
+        
+    }
+}
